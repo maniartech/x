db43b3f099c9a6308127d06a496b912c4be9b25e
kabironline <kabir.maniar@maniartech.com>
2021-03-30 11:49:47 +0530

    Worked on Base.go Did basic test to check its working Unit testing pending

---
+++ calc/base.go
@@ -0,0 +1,21 @@
+package calc
+
+import (
+	"strconv"
+
+	"github.com/maniartech/go-funcs/currency"
+)
+
+func Base(input int64, base, min_len int) string {
+	if base < 2|| base > 36 || min_len < 0 || min_len > 255 {
+		panic(currency.ErrInvalidInput)
+	}
+	value := (strconv.FormatInt(input, base))
+	if (min_len - len(value)) <= 0 {
+		return value
+	}
+	for i := min_len - (min_len - len(value)); i < min_len; i++ {
+		value = "0" + value
+	}
+	return value
+}
