540235e7ff49ebef26cd0fd3daa71822931bbf4a
Aamir Maniar <aamir.maniar@maniartech.com>
2021-04-01 11:02:35 +0530

    x.utils.is WIP

---
+++ utils/is.go
@@ -0,0 +1,23 @@
+package utils
+
+import "reflect"
+
+// IsSliceOrArray returns true if the supplied variable is
+// a slice or array. It returns false otherwise.
+// If the firat return value is true, the second return value
+// specifies whether it is slice or not.
+func IsSliceOrArray(v interface{}) (bool, int) {
+	rt := reflect.TypeOf(v)
+	switch rt.Kind() {
+	case reflect.Slice:
+	case reflect.Array:
+		return true, reflect.ValueOf(v).Len()
+	}
+	return false, -1
+}
+
+// IsFunc returns `true` if the supplied value is a function.
+// Returns `false` othwise.
+func IsFunc(v interface{}) bool {
+	return reflect.TypeOf(v).Kind() == reflect.Func
+}
