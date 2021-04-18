a1884daccd04ab53b05b09b0a90e7c75e1e65ff9
Aamir Maniar <aamir.maniar@maniartech.com>
2021-04-18 20:04:49 +0530

    Improved calc.GCD function

--- calc/gcd.go
+++ calc/gcd.go
@@ -1,6 +1,8 @@
 package calc
 
-import "github.com/maniartech/x/core"
+import (
+	"github.com/maniartech/x/utils"
+)
 
 func calcGCD(a int, b int) int {
 	if a == 0 {
@@ -9,36 +11,15 @@ func calcGCD(a int, b int) int {
 	return calcGCD(b%a, a)
 }
 
-// GCD Finds the GCD from a list of arrayed numbers
-// If the numbers are not of type int then it truncates the number
-// If the type is non-numerical then throws array
-func GCD(arr []interface{}) int {
-	//Creating a array of the type int with the size of inputed array
-	arrInt := make([]int, len(arr))
-	//Checking type of each value and storing them in new array
-	for i, value := range arr {
-		switch typedValue := value.(type) {
-		//If the type of the `arr` element is int then it gets added to the `arrInt`
-		case int:
-			arrInt[i] = typedValue
-			//If the type of the `arr` element is float64 then it gets truncated
-		case float64:
-			arrInt[i] = INT(value.(float64))
-			//If the type of `arr` is neither int nor float64 then it creates a panic
-		default:
-			panic(core.ErrInvalidInput)
+func GCD(v ...interface{}) int {
+	var result int
+	utils.ForEach(func(i int, x interface{}) {
+		ival := utils.ToInt(x)
+		if i == 0 {
+			result = ival
+			return
 		}
-	}
-	//Initalizing results with a default value
-	result := arrInt[0]
-	//Finding the GCD of 2 elements at a time
-	for i := 1; i < len(arrInt); i++ {
-		//Using the GCD value of the prev numbers we compute it with the next value
-		result = calcGCD(int(arrInt[i]), result)
-		//If the results == 1 then we return 1
-		if result == 1 {
-			return 1
-		}
-	}
+		result = calcGCD(result, ival)
+	}, v...)
 	return result
 }
