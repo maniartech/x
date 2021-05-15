243d15c6f14d2abce1debaf60282d31272c3c133
kabironline <kabir.maniar@maniartech.com>
2021-05-15 17:25:07 +0530

    Added more funcs to statistics package basic testing of new funcs are done unit testing pending

--- statistics/shorts.go
+++ statistics/shorts.go
@@ -58,8 +58,85 @@ func HarMean(v ...interface{}) float64 {
 	return calc.Divide(1, calc.Divide(1, c)*sum)
 }
 
+func ExponDist(x, lamda interface{}, cumulative bool) float64 {
+	xF := utils.ToFloat64(x)
+	lamdaF := utils.ToFloat64(lamda)
+
+	if lamdaF <= 0 || xF < 0 {
+		panic(core.ErrInvalidInput)
+	}
+
+	var val float64
+
+	if cumulative {
+		val = 1 - math.Pow(math.E, -lamdaF*xF)
+	} else {
+		val = lamdaF * math.Pow(math.E, -lamdaF*xF)
+	}
+	return val
+}
+
+//WIP
+func Kurt(x ...interface{}) float64 {
+	n := len(x)
+	if n <= 3 {
+		panic(core.ErrDivideBy0)
+	}
+	s := Vara(x...)
+	xD := Average(x...)
+	var sum float64
+	utils.ForEach(func(_ int, x interface{}) {
+		sum += math.Pow((utils.ToFloat64(x)-xD)/s, 4)
+	}, x...)
+	//*utils.ToFloat64((3*(n-1)*(n-1))/((n-2)*(n-3)))
+	return calc.Round(utils.ToFloat64((n*(n+1))/(n-1)*(n-2)*(n-3))*sum, 15)
+
+}
+
+func Intercept(x, y []interface{}) float64 {
+	if len(x) != len(y) {
+		panic(core.ErrInvalidInput)
+	}
+
+	var xD float64 = Average(x)
+	var yD float64 = Average(y)
+	b := Slope(x, y)
+	return yD - (b * xD)
+}
+
+func Slope(x, y []interface{}) float64 {
+
+	if len(x) != len(y) {
+		panic(core.ErrInvalidInput)
+	}
+
+	var n float64
+	var d float64
+	var xD float64 = Average(x)
+	var yD float64 = Average(y)
+
+	for i := 0; i < len(x); i++ {
+		n += (utils.ToFloat64(x[i]) - xD) * (utils.ToFloat64(y[i]) - yD)
+		d += (utils.ToFloat64(x[i]) - xD) * (utils.ToFloat64(x[i]) - xD)
+	}
+	return n / d
+}
+
+func Standardize(x, mean, sDev interface{}) float64 {
+	return (utils.ToFloat64(x) - utils.ToFloat64(mean)) / utils.ToFloat64(sDev)
+}
+
+func StdevP(x ...interface{}) float64 {
+	xD := Average(x...)
+	var sum float64
+	n := utils.ForEach(func(_ int, x interface{}) {
+		sum += (utils.ToFloat64(x) - xD) * (utils.ToFloat64(x) - xD)
+	}, x...)
+	return math.Sqrt(sum / utils.ToFloat64(n))
+}
+
 func Vara(v ...interface{}) float64 {
-	vA := Average(v)
+	vA := Average(v...)
 	var sum float64
 	c := utils.ForEach(func(_ int, x interface{}) {
 		sum += (utils.ToFloat64(x) - vA) * (utils.ToFloat64(x) - vA)
@@ -68,7 +145,7 @@ func Vara(v ...interface{}) float64 {
 }
 
 func DevSQ(v ...interface{}) float64 {
-	vA := Average(v)
+	vA := Average(v...)
 	var sum float64
 	utils.ForEach(func(_ int, x interface{}) {
 		sum += (utils.ToFloat64(x) - vA) * (utils.ToFloat64(x) - vA)
--- statistics/shorts_test.go
+++ statistics/shorts_test.go
@@ -8,7 +8,20 @@ import (
 )
 
 func TestShorts(t *testing.T) {
+
 	assert.Equal(t, 0.9729550745276566, statistics.Fisher(0.75))
 
 	assert.Equal(t, 0.75, statistics.FisherInv(statistics.Fisher(0.75)))
+
+	assert.Equal(t, 26.054558142482477, statistics.StdevP(1345, 1301, 1368, 1322, 1310, 1370, 1350, 1303, 1318, 1299))
+
+	// assert.Equal(t, -1.2, statistics.Kurt(1, 2, 3, 4, 5))
+	assert.Equal(t, 1.0, statistics.Slope([]interface{}{2, 4, 6}, []interface{}{1, 3, 5}))
+	assert.Equal(t, 0.6693548387096776, statistics.Slope([]interface{}{6, 5, 11, 7, 5}, []interface{}{2, 3, 9, 1, 8}))
+
+	assert.Equal(t, -1.0, statistics.Intercept([]interface{}{2, 4, 6}, []interface{}{1, 3, 5}))
+	assert.Equal(t, 0.04838709677419217, statistics.Intercept([]interface{}{6, 5, 11, 7, 5}, []interface{}{2, 3, 9, 1, 8}))
+
+	assert.Equal(t, 0.8646647167633873, statistics.ExponDist(0.2, 10, true))
+	assert.Equal(t, 1.353352832366127, statistics.ExponDist(0.2, 10, false))
 }
