2d2ee7224bb2bc4859b011e0534a72d235df7845
Aamir Maniar <aamir.maniar@maniartech.com>
2021-08-02 19:05:11 +0530

    AST Parser WIP

--- parser/cmd/main.go
+++ parser/cmd/main.go
@@ -6,12 +6,9 @@ import (
 	"os"
 	"strings"
 
-	// "github.com/maniartech/x/calc"
+	"github.com/maniartech/x/parser/ast"
 	// "github.com/maniartech/x/currency"
-	"github.com/maniartech/x/calc"
-	"github.com/maniartech/x/currency"
-	"github.com/maniartech/x/parser"
-	"github.com/maniartech/x/statistics"
+	// "github.com/maniartech/x/statistics"
 	// "github.com/maniartech/x/statistics"
 )
 
@@ -19,12 +16,9 @@ func main() {
 
 	if len(os.Args) != 2 {
 		log.Fatal("Usage: calculator 'EXPR'")
-		calc.Ceiling("12.5", 1)
-		statistics.Average(10, 20, 30)
-		currency.Digit2Word(123)
 	}
 
-	got, err := parser.ParseReader("", strings.NewReader(os.Args[1]))
+	got, err := ast.ParseReader("", strings.NewReader(os.Args[1]))
 
 	if err != nil {
 		log.Fatal(err)
